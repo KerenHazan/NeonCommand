7d11638b0bcc601d5010302a22193816c942c960
Keren Hazan <kerenha2@mta.ac.il>
2026-09-20 20:53:35 +0300

    Add expanding explosion sequence

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    [SerializeField] private float maxRadius = 1.8f;
+    [SerializeField] private float expandDuration = 0.25f;
+    [SerializeField] private float holdDuration = 0.2f;
+
+    public void Play(Vector3 position)
+    {
+        StopAllCoroutines();
+        transform.position = position;
+        transform.localScale = Vector3.one * 0.02f;
+        gameObject.SetActive(true);
+        StartCoroutine(ExplosionSequence());
+    }
+
+    private IEnumerator ExplosionSequence()
+    {
+        float elapsed = 0f;
+        float diameter = maxRadius * 2f;
+
+        while (elapsed < expandDuration)
+        {
+            float size = Mathf.SmoothStep(0.02f, diameter, elapsed / expandDuration);
+            transform.localScale = Vector3.one * size;
+            yield return null;
+            elapsed += Time.deltaTime;
+        }
+
+        transform.localScale = Vector3.one * diameter;
+        yield return new WaitForSeconds(holdDuration);
+        gameObject.SetActive(false);
+    }
+}
